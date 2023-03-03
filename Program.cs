9a796da453db6ef7c7234f85d8837bb1d323f00a
romanzvezdochet <126814224+romanzvezdochet@users.noreply.github.com>
2023-03-03 20:19:44 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,22 @@ namespace Chess
 {
     class Program
     {
+        static int[] input(string promptx, string prompty)
+        {
+
+            Console.Write(promptx);
+            int x = Convert.ToByte(Console.ReadLine());
+            Console.Write(prompty);
+            int y = Convert.ToByte(Console.ReadLine());
+            int[] a = { x, y };
+            return a;
+        }
+
         static void Main()
         {
-            byte KorolX;
-            byte KorolY;
-            byte Slonx;
-            byte Slony;
-            byte Ladyax;
-            byte LadyaY;
-
+            int[] Korol = new int[2];
+            int[] Slon = new int[2];
+            int[] Ladya = new int[2];
             Console.Write("Шахматная задача с определением угрозы королю, шахматное поле представляет собой поле 8 x 8 клеток,\n так что числа в вводе вводите от 1 до 8 включительно \n");
             Console.WriteLine("1  2  3  4  5  6  7  8");
             for (int i = 8; i > 0; i--)
@@ -40,90 +47,76 @@ namespace Chess
             }
             Console.WriteLine();
             Console.WriteLine("введите позиции для Короля, сначала по оси x - горизонтали, потом по оси y - вертикали");
-            Console.Write("KorolX= ");
-            KorolX = Convert.ToByte(Console.ReadLine());
-            Console.Write("KorolY= ");
-            KorolY = Convert.ToByte(Console.ReadLine());
-            while (KorolX < 1 || KorolX > 8 || KorolY < 1 || KorolY > 8) // король за полем или на неположеном месте
+            Korol = input("Король по горизонтали= ", "Король по вертикали= ");
+            while (Korol[0] <= 0 || Korol[0] >= 9 || Korol[1] <= 0 || Korol[1] >= 9) // король за полем или на неположеном месте
             {
                 Console.WriteLine("Ошибка! Фигура за полем! Введите позицию короля снова.");
                 Console.WriteLine("введите позиции для Короля, сначала по оси x - горизонтали, потом по оси y - вертикали");
-                Console.Write("KorolX= ");
-                KorolX = Convert.ToByte(Console.ReadLine());
-                Console.Write("KorolY= ");
-                KorolY = Convert.ToByte(Console.ReadLine());
-
+                Korol = input("Король по горизонтали= ", "Король по вертикали= ");
             }
-
             Console.WriteLine("введите позиции для Слона, сначала по оси x - горизонтали, потом по оси y - вертикали");
-            Console.Write("SlonX= ");
-            Slonx = Convert.ToByte(Console.ReadLine());
-            Console.Write("SlonY= ");
-            Slony = Convert.ToByte(Console.ReadLine());
-            while (Slonx < 1 || Slonx > 8 || Slony < 1 || Slony > 8) // слон за полем или на неположеном месте
+            Slon = input("Слон по горизонтали= ", "Слон по вертикали= ");
+            while (Slon[0] <= 0 || Slon[0] >= 9 || Slon[1] <= 0 || Slon[1] >= 9) // слон за полем или на неположеном месте
             {
                 Console.WriteLine("Ошибка! Фигура за полем!");
                 Console.WriteLine("введите позиции для Слона, сначала по оси x - горизонтали, потом по оси y - вертикали");
-                Console.Write("SlonX= ");
-                Slonx = Convert.ToByte(Console.ReadLine());
-                Console.Write("SlonY= ");
-                Slony = Convert.ToByte(Console.ReadLine());
+                Slon = input("Слон по горизонтали= ", "Слон по вертикали= ");
+            }
+            while (Slon[0] == Korol[0] && Slon[1] == Korol[1])
+            {
+                Console.WriteLine("Ошибка! Поле занято!");
+                Console.WriteLine("введите позиции для Слона, сначала по оси x - горизонтали, потом по оси y - вертикали");
+                Slon = input("Слон по горизонтали= ", "Слон по вертикали= ");
             }
             Console.WriteLine("введите позиции для Ладьи, сначала по оси x - горизонтали, потом по оси y - вертикали");
-            Console.Write("LadyaX= ");
-            Ladyax = Convert.ToByte(Console.ReadLine());
-            Console.Write("LadyaY= ");
-            LadyaY = Convert.ToByte(Console.ReadLine());
-            while (Ladyax < 1 || Ladyax > 8 || LadyaY < 1 || LadyaY > 8) // ладья за полем или на неположеном месте
+            Ladya = input("Ладья по горизонтали= ", "Ладья по вертикали= ");
+            while (Ladya[0] <= 0 || Ladya[0] >= 9 || Ladya[1] <= 0 || Ladya[1] >= 9) // ладья за полем или на неположеном месте
             {
                 Console.WriteLine("Ошибка! Фигура за полем!");
                 Console.WriteLine("введите позиции для Ладьи, сначала по оси x - горизонтали, потом по оси y - вертикали");
-                Console.Write("LadyaX= ");
-                Ladyax = Convert.ToByte(Console.ReadLine());
-                Console.Write("LadyaY= ");
-                LadyaY = Convert.ToByte(Console.ReadLine());
+                Ladya = input("Ладья по горизонтали= ", "Ладья по вертикали= ");
             }
-            // если король совпадает обеими координатами с другой фигурой выдаст ошибку
-            if ((KorolX == Slonx && KorolY == Slony) || (KorolX == Ladyax && KorolY == LadyaY))
+            while ((Ladya[0] == Korol[0] && Ladya[1] == Korol[1]) || (Slon[0] == Ladya[0] && Slon[1] == Ladya[1]))
             {
-                Console.WriteLine("Ошибка! Фигура на фигуре!");
-                return;
+                Console.WriteLine("Ошибка! Поле занято!");
+                Console.WriteLine("введите позиции для Ладьи, сначала по оси x - горизонтали, потом по оси y - вертикали");
+                Ladya = input("Ладья по горизонтали= ", "Ладья по вертикали= ");
             }
-            // если слон совпадает обеими координатами с другой фигурой выдаст ошибку
+            /*// если слон совпадает обеими координатами с другой фигурой выдаст ошибку
             if ((Slonx == KorolX && Slony == KorolY) || (Slonx == Ladyax && Slony == LadyaY))
             {
                 Console.WriteLine("Ошибка! Фигура на фигуре!");
                 return;
-            }
+            }*/
             Console.WriteLine("Фигуры раставлены");
-            Console.WriteLine("Король [{0},{1}], Слон [{2},{3}], Ладья [{4},{5}]", KorolX, KorolY, Slonx, Slony, Ladyax, LadyaY);
+            Console.WriteLine("Король [{0},{1}], Слон [{2},{3}], Ладья [{4},{5}]", Korol[0], Korol[1], Slon[0], Slon[1], Ladya[0], Ladya[1]);
             Console.WriteLine("угроза такова:");
             bool Slonperedladya = false, ladyaperedslon = false, s = false, l = false, k = false;
-            if ((Ladyax == Slonx) || (LadyaY == Slony))
+            if ((Ladya[0] == Slon[0]) || (Ladya[1] == Slon[1]))
             {
-                if (((Slonx > KorolX && Slonx < Ladyax) || (Slonx < KorolX && Slonx > Ladyax)) ||
-                ((Slony > KorolY && Slony < LadyaY) || (Slony < KorolY && Slony > LadyaY)))
+                if (((Slon[0] > Korol[0] && Slon[0] < Ladya[0]) || (Slon[0] < Korol[0] && Slon[0] > Ladya[0])) ||
+                ((Slon[1] > Korol[1] && Slon[1] < Ladya[1]) || (Slon[1] < Korol[1] && Slon[1] > Ladya[1])))
                 {
                     Slonperedladya = true; // слон перед ладьей и преграждает ей шах
                 }
             }
-            if (Math.Abs(Ladyax - Slonx) == Math.Abs(LadyaY - Slony))
+            if (Math.Abs(Ladya[0] - Slon[0]) == Math.Abs(Ladya[1] - Slon[1]))
             {
-                if (Math.Abs(Ladyax - Slonx) < Math.Abs(KorolX - Slonx))
+                if (Math.Abs(Ladya[0] - Slon[0]) < Math.Abs(Korol[0] - Slon[0]))
                 {
                     ladyaperedslon = true; // ладья на пути шаха слона, нет шаха
                 }
             }
-            if (Math.Abs(KorolX - Slonx) == Math.Abs(KorolY - Slony))
+            if (Math.Abs(Korol[0] - Slon[0]) == Math.Abs(Korol[1] - Slon[1]))
             {
                 s = true;
             }
-            if (KorolX == Ladyax || KorolY == LadyaY)
+            if (Korol[0] == Ladya[0] || Korol[1] == Ladya[1])
             {
                 l = true;
             }
             // нет угрозы королю
-            if (Math.Abs(KorolX - Slonx) != Math.Abs(KorolY - Slony) && (KorolX != Ladyax && KorolY != LadyaY))
+            if (Math.Abs(Korol[0] - Slon[0]) != Math.Abs(Korol[1] - Slon[1]) && (Korol[0] != Ladya[0] && Korol[1] != Ladya[1]))
             {
                 k = true;
             }
